c9a937304f74d1547396a5b9a2cf7ffc7ae26843
NK-Drakula <emmyressure2@gmail.com>
2023-03-06 03:34:12 +0100

    rebuilding

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <script setup>
-import Logo from '@/components/header/LogoHead.vue'
+import LogoHead from '@/components/header/LogoHead.vue'
 import NavBar from '@/components/header/NavBar.vue'
 
 </script>
@@ -10,7 +10,7 @@ import NavBar from '@/components/header/NavBar.vue'
       class="px-2 sm:px-5 py-2.5 bg-gradient-to-r from-indigo-800 to-primary fixed w-screen dark:bg-gradient-to-r dark:from-neutral-900 dark:to-neutral-800 z-20 top-0 left-0 right-0 shadow shadow-zinc-800 ">
       <div class="">
         <div class="flex flex-wrap items-center justify-between">
-          <Logo />
+          <LogoHead />
           <NavBar />
         </div>
       </div>
