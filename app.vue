831a5bd9d7aef8174c140a0303d3a6571c8d3a41
NK-Drakula <emmyressure2@gmail.com>
2023-05-26 14:55:22 +0100

    Upgraded

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <section
+    class="min-h-screen bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
+    <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+      <div class="animate-pulse text-white leading-none mb-10">
+        <h1 class="text-4xl font-extrabold tracking-tight md:text-5xl lg:text-6xl">
+          Maintenance Mode
+        </h1>
+        <h3 class="text-center font-bold text-3xl text-bold md:text-4xl lg:text-5xl">Coming Soon!!!</h3>
+      </div>
+
+      <div class="flex gap-3 items-center justify-center">
+        <!-- Image -->
+        <div class="">
+          <img src="./assets/images/favorite.png" alt="">
+        </div>
+        <!-- Name -->
+        <h2 class="uppercase font-extrabold text-3xl md:text-4xl lg:text-5xl text-white">kembit</h2>
+      </div>
+
+    </div>
+  </section>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './node_modules/flowbite/**/*.js'],
+  content: [    
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
   theme: {
-    extend: {
-      screens: {
-        sm: '480px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px'
-      },
-      colors: {
-        primary: '#4648EC',
-      }
-    }
+    extend: {},
   },
-  // eslint-disable-next-line no-undef
-  plugins: [require('flowbite/plugin')]
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
+
